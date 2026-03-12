e6e67c47185e2e619ee965c15b3eaa4a5e795786
FUVESZTAR <fuvesztar@gmail.com>
2026-03-12 12:41:22 +0100

    Updated loadPlantData function to use Google Sheets API and added parseGoogleSheets function.

---
+++ csv-utils.js
@@ -0,0 +1,19 @@
+function loadPlantData() {
+    const sheetId = '1QHJzWztssucMlnozk2tV9ym6gLedgDj4Zh3DzCTFWCY';
+    const sheetName = 'PlantDataSheet';
+    const url = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?tqx=out:json`;  
+
+    return fetch(url)
+        .then(response => response.text())
+        .then(data => parseGoogleSheets(data));
+}
+
+function parseGoogleSheets(data) {
+    const json = data.substr(47).slice(0, -2); // Remove the callback wrapper
+    const parsedData = JSON.parse(json);
+    const rows = parsedData.table.rows;
+
+    return rows.map(row => {
+        return row.c.map(cell => (cell ? cell.v : '')).join('\t'); // Assuming tab separation
+    });
+}
